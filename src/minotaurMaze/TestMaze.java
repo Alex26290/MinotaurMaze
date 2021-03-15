c5fc07d0c135d50fc47c195ce70cc26c664c3696
Alex Gubaydulin <agbox99@mail.r>
2021-03-15 17:35:09 +0400

    Norm Version

--- src/minotaurMaze/TestMaze.java
+++ src/minotaurMaze/TestMaze.java
@@ -13,7 +13,8 @@ import java.util.*;
 /**
  * @author DNS
  */
-public class TestMaze{
+public class TestMaze {
+    static int maxFlow = 0;
     Cell cell = new Cell();
 
     public static List<Coord> visited = new ArrayList<>();
@@ -32,7 +33,9 @@ public class TestMaze{
             System.out.println("Поток = 0, минотавр находится у края лабиринта");
         } else {
             LinkedHashMap<Edge, Integer> flow = getMaxFlow(g, g.getIstok().getCoord(), g.getStok().getCoord());
-            System.out.println("Поток = " + getFlowSize(flow, g, g.getIstok().getCoord()));
+//            System.out.println("Поток = " + getFlowSize(flow, g, g.getIstok().getCoord()));
+            System.out.println("Поток = " + maxFlow);
+            maxFlow = 0;
         }
     }
     public void startMaze() {
@@ -43,7 +46,6 @@ public class TestMaze{
             System.out.println("Поток = 0, минотавр находится у края лабиринта");
         } else {
             LinkedHashMap<Edge, Integer> flow = getMaxFlow(g, g.getIstok().getCoord(), g.getStok().getCoord());
-            System.out.println("Поток = " + getFlowSize(flow, g, g.getIstok().getCoord()));
         }
     }
 
@@ -65,23 +67,28 @@ public class TestMaze{
         // The path from source to sink that is found in each iteration
         LinkedList<Edge> path;
         // The flow, i.e. the capacity of each edge that is actually used
-        HashMap<Edge, Integer> flow = new LinkedHashMap<Edge, Integer>();
+        LinkedHashMap<Edge, Integer> flow = new LinkedHashMap<Edge, Integer>();
         // Create initial empty flow.
         for (Edge e : g.getEdges()) {
-            if (e.getCapacity() == 1) {
-                flow.put(e, 1);
-            }
+            flow.put(e, 0);
         }
 
-        // Алгоритм нахождения максимального потока
+        // The Algorithm itself
+        System.out.println("flow = " + flow);
         while ((path = bfs(g, source, sink, flow)) != null) {
+            maxFlow++;
             System.out.println("path = " + path);
-            // Нахождение потока
+            // Activating this output will illustrate how the algorithm works
+            // System.out.println(path);
+            // Find out the flow that can be sent on the found path.
             int minCapacity = Integer.MAX_VALUE;
-            Coord lastNode = source;
+            Object lastNode = source;
             for (Edge edge : path) {
-                int c = 0;
-//Условие для нахождения действительного направления граней
+                int c;
+                // Although the edges are directed they can be used in both
+                // directions if the capacity is partially used, so this if
+                // statement is necessary to find out the edge's actual
+                // direction.
                 if (edge.getStart().equals(lastNode)) {
                     c = edge.getCapacity() - flow.get(edge);
                     lastNode = edge.getTarget();
@@ -94,9 +101,11 @@ public class TestMaze{
                 }
             }
 
-//Изменяем поток всех граней пути в соответствии со значением, рассчитанным выше
+            // Change flow of all edges of the path by the value calculated
+            // above.
             lastNode = source;
             for (Edge edge : path) {
+                // If statement like above
                 if (edge.getStart().equals(lastNode)) {
                     flow.put(edge, flow.get(edge) + minCapacity);
                     lastNode = edge.getTarget();
@@ -106,110 +115,14 @@ public class TestMaze{
                 }
             }
         }
-        return (LinkedHashMap<Edge, Integer>) flow;
+        return flow;
     }
 
-    static int getFlowSize(LinkedHashMap<Edge, Integer> flow, Graph g,
-                            Coord source) {
-        HashMap<Coord, Integer> currentFlow = new HashMap<>();
-        Node sourceNode = g.getNode(source.toString());
-
-        List<LinkedList<Edge>> routes = new LinkedList<>();
-        LinkedList<Edge> currentRoute = new LinkedList<>();
-        LinkedList<Edge> usedEdges = new LinkedList<>();
-        LinkedList<Edge> unUsedEdges = new LinkedList<>();
-        int maximumFlow = 0;
-        Edge currentEdge = null;
-        for (Map.Entry entry : flow.entrySet()) {
-            Edge nextEdge = (Edge) entry.getKey();
-            usedEdges.add(nextEdge);
-        }
-        currentRoute = new LinkedList<>();
-        Coord currentCoord;
-        for (Coord coord : Ranges.getCoordsAround(source)) {
-            if (Cell.cellMap.get(coord) == Box.OPENED) {
-                currentCoord = coord;
-                for (int i = 0; i < usedEdges.size(); i++) {
-                    currentEdge = usedEdges.get(i);
-                    if (!currentEdge.getTarget().equals(g.getStok().getCoord())) {
-                        if (currentCoord.equals(currentEdge.getStart())) {
-                            currentRoute.add(currentEdge);
-                            currentRoute.add(currentEdge);
-                            currentCoord = currentEdge.getTarget();
-                            currentRoute = new LinkedList<>();
-                            routes.add(currentRoute);
-                        }
-                    } else if (currentEdge.getTarget().equals(g.getStok().getCoord())) {
-                        if (currentCoord.equals(currentEdge.getStart())) {
-                            currentCoord = currentEdge.getTarget();
-                            currentRoute.add(currentEdge);
-                            System.out.println("test2");
-                            routes.add(currentRoute);
-                            currentRoute = new LinkedList<>();
-                        }
-                    }
-                }
-            }
-        }
-        maximumFlow = routes.size();
-//        int crosses = checkRoutes(routes);
-        for (LinkedList<Edge> list : routes) {
-            for (Edge edge : list) {
-                System.out.println(edge);
-            }
-            System.out.println("\n");
-        }
-        if (maximumFlow > sourceNode.getOutLeadingOrder()) {
-            maximumFlow = sourceNode.getOutLeadingOrder();
-        }
-        return maximumFlow;
-    }
-
-    private static boolean hasNextEdge(Coord start, LinkedList<Edge> usedEdges) {
-        boolean hasNextEdge = false;
-        for (Edge edge : usedEdges) {
-            if (edge.getStart().equals(start)) {
-                hasNextEdge = true;
-            }
-        }
-        return hasNextEdge;
-    }
-
-    private static int checkRoutes(List<LinkedList<Edge>> routes) {
-        int countOfCrosses = 0;
-        boolean ifFound = false;
-        for (int i = 0; i < routes.size() - 1; i++) {
-            LinkedList<Edge> currentRoute = routes.get(i);
-            System.out.println("Список граней = " + currentRoute);
-            LinkedList<Edge> nextRoute = routes.get(i + 1);
-            System.out.println("Список граней 2 = " + nextRoute);
-            for (int k = 0; k < currentRoute.size(); k++) {
-                if (ifFound) {
-                    ifFound = false;
-                    break;
-                }
-                System.out.println("currentRoute.get() = " + currentRoute.get(k).toString());
-                Edge current = currentRoute.get(k);
-                for (int n = 0; n < nextRoute.size(); n++) {
-                    Edge next = nextRoute.get(n);
-                    System.out.println("nextRoute.get(n) = " + nextRoute.get(n).toString());
-                    System.out.println(current.toString().equalsIgnoreCase(next.toString()));
-                    if (current.toString().equalsIgnoreCase(next.toString())) {
-                        countOfCrosses++;
-                        ifFound = true;
-                        break;
-                    }
-                }
-            }
-        }
-        System.out.println("Количество пересечений = " + countOfCrosses);
-        return countOfCrosses;
-    }
 
     static LinkedList<Edge> bfs(Graph g, Coord start, Coord target,
                                 HashMap<Edge, Integer> flow) {
         // The edge by which a node was reached.
-        HashMap<Coord, Edge> parent = new HashMap<>();
+        HashMap<Coord, Edge> parent = new HashMap<Coord, Edge>();
         // All outer nodes of the current search iteration.
         LinkedList<Coord> fringe = new LinkedList<>();
         // We need to put the start node into those two.
@@ -218,57 +131,43 @@ public class TestMaze{
         // The actual algorithm
         all:
         while (!fringe.isEmpty()) {
-            System.out.println("fringe не пустой, в нём есть нод истока");
             // This variable is needed to prevent the JVM from having a
             // concurrent modification
             LinkedList<Coord> newFringe = new LinkedList<>();
             // Iterate through all nodes in the fringe.
-            System.out.println("fringe = " + fringe);
             for (Coord coord : fringe) {
-                System.out.println("В цикле по координатам");
-                System.out.println("\n");
-                System.out.println("Получаем нод из fringe - " + g.getNode(coord.toString()));
+//                Node node = g.getNode(nodeID);
                 Node node = g.getNode(coord.toString());
                 // Iterate through all the edges of the node.
-                System.out.println("Количество граней для нода = " + node.getOutLeadingOrder());
                 for (int i = 0; i < node.getOutLeadingOrder(); i++) {
                     Edge e = node.getEdge(i);
                     // Only add the node if the flow can be changed in an out
                     // leading direction. Also break, if the target is reached.
                     if (e.getStart().equals(coord)
                             && !parent.containsKey(e.getTarget())
-                            && !(visited.contains(e.getStart()) || visited.contains(e.getTarget()))
                             && flow.get(e) < e.getCapacity()) {
-                        System.out.println("Test Point 3");
-                        parent.put(e.getStart(), e);
                         parent.put(e.getTarget(), e);
                         if (e.getTarget().equals(target)) {
-                            System.out.println("Найден сток");
+//                            break;
                             break all;
                         }
-                        System.out.println("Заменяем стартовый нод на таргет из грани");
                         newFringe.add(e.getTarget());
-                        visited.add(e.getTarget());
                     } else if (e.getTarget().equals(coord)
                             && !parent.containsKey(e.getStart())
-                            && !(visited.contains(e.getStart()) || visited.contains(e.getTarget()))
                             && flow.get(e) > 0) {
                         parent.put(e.getStart(), e);
-                        parent.put(e.getTarget(), e);
                         if (e.getStart().equals(target)) {
-                            System.out.println("Найден сток");
+//                            break;
                             break all;
                         }
                         newFringe.add(e.getStart());
-                        visited.add(e.getTarget());
                     }
                 }
             }
             // Replace the fringe by the new one.
             fringe = newFringe;
         }
-        System.out.println("\n");
-        System.out.println("fringe пуст? - " + fringe.isEmpty());
+
         // Return null, if no path was found.
         if (fringe.isEmpty()) {
             return null;
@@ -279,13 +178,18 @@ public class TestMaze{
         while (!node.equals(start)) {
             Edge e = parent.get(node);
             path.addFirst(e);
-            if (e.getStart().equals(node)) {
-                node = e.getTarget();
-            } else {
-                node = e.getStart();
+            if (e != null) {
+                if (e.getStart() != null) {
+                    if (e.getStart().equals(node)) {
+                        node = e.getTarget();
+                    } else {
+                        node = e.getStart();
+                    }
+                }
+            }else{
+                return null;
             }
         }
-        // Возвращаем путь
         return path;
     }
 }
